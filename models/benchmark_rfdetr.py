d5d0aadf87cb25c6b6734957625346ade0c25c7b
Isaac Robinson <isaac@roboflow.com>
2025-07-22 01:52:22 +0000

    benchmark RFDETR via TRT

--- models/benchmark_rfdetr.py
+++ models/benchmark_rfdetr.py
@@ -4,8 +4,10 @@ from PIL import Image
 import numpy as np
 import io
 import requests
+import os
 
 from onnx_inference import ONNXInference
+from trt_inference import TRTInference, build_engine
 from evaluation import evaluate
 
 
@@ -18,45 +20,61 @@ def cxcywh_to_xyxy(boxes):
     return boxes
 
 
+def preprocess_image(image: torch.Tensor, image_input_shape: tuple[int, int]) -> torch.Tensor:
+    if len(image.shape) == 3:
+        image = image.unsqueeze(0)
+    
+    means = torch.tensor([0.485, 0.456, 0.406], device=image.device).view(1, 3, 1, 1)
+    stds = torch.tensor([0.229, 0.224, 0.225], device=image.device).view(1, 3, 1, 1)
+
+    image = TF.normalize(image, means, stds)
+    image = TF.resize(image, image_input_shape[2:])
+    return image
+
+
+def postprocess_output(outputs: dict[str, torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    bboxes = outputs["dets"]
+    out_logits = outputs["labels"]
+    scores = out_logits.sigmoid()
+
+    topk_values, topk_indexes = torch.topk(scores.view(scores.shape[0], -1), 300, dim=1)
+    scores = topk_values
+    topk_boxes = topk_indexes // out_logits.shape[2]
+    labels = topk_indexes % out_logits.shape[2]
+    bboxes = torch.gather(bboxes, 1, topk_boxes.unsqueeze(-1).repeat(1,1,4))
+
+    bboxes = cxcywh_to_xyxy(bboxes)
+
+    return bboxes.contiguous(), labels.contiguous(), scores.contiguous()
+
+
 class RFDETRONNXInference(ONNXInference):
-    def __init__(self, model_path: str, image_input_name: str|None=None):
-        super().__init__(model_path, image_input_name)
-
-        self.means = torch.tensor([0.485, 0.456, 0.406], device="cuda").view(1, 3, 1, 1)
-        self.stds = torch.tensor([0.229, 0.224, 0.225], device="cuda").view(1, 3, 1, 1)
-
     def preprocess(self, input_image: torch.Tensor) -> torch.Tensor:
-        if len(input_image.shape) == 3:
-            input_image = input_image.unsqueeze(0)
-
-        input_image = TF.normalize(input_image, self.means, self.stds)
-        input_image = TF.resize(input_image, self.image_input_shape[2:])
-
-        return input_image
+        return preprocess_image(input_image, self.image_input_shape)
     
     def postprocess(self, outputs: dict[str, torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        bboxes = outputs["dets"]
-        out_logits = outputs["labels"]
-        scores = out_logits.sigmoid()
+        return postprocess_output(outputs)
 
-        topk_values, topk_indexes = torch.topk(scores.view(scores.shape[0], -1), 300, dim=1)
-        scores = topk_values
-        topk_boxes = topk_indexes // out_logits.shape[2]
-        labels = topk_indexes % out_logits.shape[2]
-        bboxes = torch.gather(bboxes, 1, topk_boxes.unsqueeze(-1).repeat(1,1,4))
 
-        bboxes = cxcywh_to_xyxy(bboxes)
-
-        return bboxes.contiguous(), labels.contiguous(), scores.contiguous()
+class RFDETRTRTInference(TRTInference):
+    def preprocess(self, input_image: torch.Tensor) -> torch.Tensor:
+        return preprocess_image(input_image, self.image_input_shape)
+    
+    def postprocess(self, outputs: dict[str, torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        return postprocess_output(outputs)
 
 
 if __name__ == "__main__":
     model_path = "rf-detr-nano.onnx"
+    engine_path = "rf-detr-nano.engine"
     coco_dir = "/home/isaac/cocodir/val2017"
     coco_annotations_file_path = "/home/isaac/cocodir/annotations/instances_val2017.json"
 
-    inference = RFDETRONNXInference(model_path)
+    # onnx_inference = RFDETRONNXInference(model_path)
+    if not os.path.exists(engine_path):
+        build_engine(model_path, engine_path)
+    trt_inference = RFDETRTRTInference(engine_path)
 
-    evaluate(inference, coco_dir, coco_annotations_file_path)
+    evaluate(trt_inference, coco_dir, coco_annotations_file_path)
 
-    inference.print_latency_stats()
+    trt_inference.print_latency_stats()
---
+++ trt_inference.py
@@ -0,0 +1,120 @@
+import tensorrt as trt
+import torch
+import numpy as np
+
+from profiler import CUDAProfiler
+
+
+def build_engine(model_path, engine_path, use_fp16=False):
+    logger = trt.Logger(trt.Logger.INFO)
+    builder = trt.Builder(logger)
+    
+    config = builder.create_builder_config()
+    if use_fp16:
+        config.set_flag(trt.BuilderFlag.FP16)
+
+    EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+    network = builder.create_network(EXPLICIT_BATCH)
+
+    parser = trt.OnnxParser(network, logger)
+
+    with open(model_path, "rb") as f:
+        model_data = f.read()
+    parser.parse(model_data)
+
+    print(f"Building engine from {model_path} to {engine_path}")
+    engine = builder.build_serialized_network(network, config)
+    print(f"Engine built successfully")
+
+    with open(engine_path, "wb") as f:
+        f.write(engine)
+
+    return engine
+
+
+class TRTInference:
+    def __init__(self, engine_path: str, image_input_name: str|None=None):
+        logger = trt.Logger()
+        trt.init_libnvinfer_plugins(logger, "")
+
+        with open(engine_path, "rb") as f:
+            self.engine = trt.Runtime(logger).deserialize_cuda_engine(f.read())
+        
+        self.context = self.engine.create_execution_context()
+
+        self.input_names = [self.engine.get_binding_name(i) for i in range(self.engine.num_bindings) if self.engine.binding_is_input(i)]
+        self.output_names = [self.engine.get_binding_name(i) for i in range(self.engine.num_bindings) if not self.engine.binding_is_input(i)]
+        self.output_shapes = [list(self.engine.get_binding_shape(name)) for name in self.output_names]
+
+        self.initialize_bindings()
+
+        # Cache binding indices
+        self.input_binding_idxs = {
+            name: self.engine.get_binding_index(name) 
+            for name in self.input_names
+        }
+        self.output_binding_idxs = {
+            name: self.engine.get_binding_index(name) 
+            for name in self.output_names
+        }
+
+        if len(self.input_names) != 1 and image_input_name is None:
+            raise ValueError("Model has multiple inputs, but no image input name was provided")
+        elif len(self.input_names) == 1 and image_input_name is not None:
+            assert image_input_name in self.input_names, f"Image input name {image_input_name} not found in model inputs"
+        
+        self.image_input_name = image_input_name if image_input_name is not None else self.input_names[0]
+        self.image_input_shape = self.engine.get_binding_shape(self.image_input_name)
+
+        self.profiler = CUDAProfiler()
+    
+    def preprocess(self, input_image: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def construct_bindings(self, input_image: torch.Tensor) -> list[int]:
+        # Construct bindings for the input and output tensors
+        if len(self.input_names) != 1:
+            raise RuntimeError("Default implementation only supports models with a single input, please subclass and implement this method")
+        
+        input_image = input_image.contiguous()
+
+        bindings = [None] * self.engine.num_bindings
+
+        for name, binding_idx in self.output_binding_idxs.items():
+            bindings[binding_idx] = self.binding_ptrs[name]
+        
+        bindings[self.input_binding_idxs[self.image_input_name]] = input_image.data_ptr()
+
+        return bindings
+    
+    def initialize_bindings(self):
+        self.bindings = {}
+        self.binding_ptrs = {}
+        for i in range(self.engine.num_bindings):
+            name = self.engine.get_binding_name(i)
+            if not self.engine.binding_is_input(i):
+                shape = self.engine.get_binding_shape(name)
+                dtype = trt.nptype(self.engine.get_binding_dtype(name))
+                self.bindings[name] = torch.from_numpy(np.empty(shape, dtype=dtype)).cuda()
+                self.binding_ptrs[name] = self.bindings[name].data_ptr()
+    
+    def postprocess(self, output_tensors: dict[str, torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        # Postprocess the output tensors into bbox, class, and score
+        # bbox must be in normalized coordinates (0-1) and in xyxy format
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def infer(self, input_image: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        input_image = self.preprocess(input_image)
+
+        bindings = self.construct_bindings(input_image)
+
+        with self.profiler.profile():
+            success = self.context.execute_v2(bindings)
+            assert success, "Execution failed"
+                
+        outputs = {name: self.bindings[name].clone() for name in self.output_names}
+
+        return self.postprocess(outputs)
+
+    def print_latency_stats(self):
+        self.profiler.print_stats()
